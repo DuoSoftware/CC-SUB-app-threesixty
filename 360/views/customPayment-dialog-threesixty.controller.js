a1e6f10a214aeb44afd5162194c23251dcfbd523
Kasun <kasun.w@duosoftware.com>
2017-05-16 20:02:07 +0530

    Version 6.1.0.3

--- 360/views/customPayment-dialog-threesixty.controller.js
+++ 360/views/customPayment-dialog-threesixty.controller.js
@@ -24,30 +24,37 @@
 	  $scope.isTimelineDialogLoaded = isTimelineDialogLoaded;
 	  $scope.docType = docType;
 	  $scope.selectedDoc = selectedDoc;
-	  $scope.printDiv = function(divName) {
-
-      if(vm.selectedPayment.companyLogo == undefined || vm.selectedPayment.companyLogo == ''){
-        var printContents = document.getElementById(divName).innerHTML;
-        var popupWin = window.open('', '_blank', 'width=1800,height=700');
-        popupWin.document.open();
-        popupWin.document.write('<html><head>' +
-          "<link href='app/main/360/dialogs/compose/print-view.css' rel='stylesheet' type='text/css'><link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>"+
-          '</head><body>' + printContents + '<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script><script>$(document).ready(function (){window.print()});</script></body></html>');
-        //window.print();
-        popupWin.document.close();
-      }else{
-        var printContents = document.getElementById(divName).innerHTML;
-        console.log(printContents);
-        var popupWin = window.open('', '_blank', 'width=1800,height=700');
-        popupWin.document.open();
-        popupWin.document.write('<html><head>' +
-          "<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>"+
-          "<link href='app/main/360/dialogs/compose/print-view2.css' rel='stylesheet' type='text/css'>"+
-          '</head><body>' + printContents + '<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script><script>$(document).ready(function (){window.print()});</script></body></html>');
-        //window.print();
-        popupWin.document.close();
-      }
-    }
+	  $scope.printDiv = function () {
+		  var printContent = document.getElementById('print-content');
+		  var popupWin = window.open('', '_blank', 'width=1000,height=700');
+		  popupWin.document.open();
+		  popupWin.document.write('<html><head><link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css"><link href="app/main/360/dialogs/compose/print-view.css" rel="stylesheet" type="text/css"></head><body>' + printContent.innerHTML + '<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script><script>$(document).ready(function (){window.print()});</script></body></html>');
+		  popupWin.document.close();
+	  };
+	 //  $scope.printDiv = function(divName) {
+	//
+    //   if(vm.selectedPayment.companyLogo == undefined || vm.selectedPayment.companyLogo == ''){
+    //     var printContents = document.getElementById(divName).innerHTML;
+    //     var popupWin = window.open('', '_blank', 'width=1800,height=700');
+    //     popupWin.document.open();
+    //     popupWin.document.write('<html><head>' +
+    //       "<link href='app/main/360/dialogs/compose/print-view.css' rel='stylesheet' type='text/css'><link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>"+
+    //       '</head><body>' + printContents + '<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script><script>$(document).ready(function (){window.print()});</script></body></html>');
+    //     //window.print();
+    //     popupWin.document.close();
+    //   }else{
+    //     var printContents = document.getElementById(divName).innerHTML;
+    //     console.log(printContents);
+    //     var popupWin = window.open('', '_blank', 'width=1800,height=700');
+    //     popupWin.document.open();
+    //     popupWin.document.write('<html><head>' +
+    //       "<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>"+
+    //       "<link href='app/main/360/dialogs/compose/print-view2.css' rel='stylesheet' type='text/css'>"+
+    //       '</head><body>' + printContents + '<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script><script>$(document).ready(function (){window.print()});</script></body></html>');
+    //     //window.print();
+    //     popupWin.document.close();
+    //   }
+    // }
 
     $charge.commondata().getDuobaseFieldDetailsByTableNameAndFieldName("CTS_GeneralAttributes","DecimalPointLength").success(function(data) {
       $scope.decimalPoint=parseInt(data[0].RecordFieldData);
