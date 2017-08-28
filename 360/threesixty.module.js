5bc14be2a1a61133edb35271ed515ed8150ba726
Kasun <kasun.w@duosoftware.com>
2017-08-28 17:23:53 +0530

    Revert test code

--- 360/threesixty.module.js
+++ 360/threesixty.module.js
@@ -2,58 +2,42 @@
 // App : 360
 // Owner  : Gihan Herath
 // Last changed date : 2017/08/24
-// Version : 6.1.0.13
+// Version : 6.1.0.12
 // Modified By : Kasun
 /////////////////////////////////
 
 (function ()
 {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('app.threesixty', [])
-        .config(config)
-        .filter('parseDate',parseDateFilter)
-        .filter('numberFixedLen',numberFixedLength)
+	angular
+		.module('app.threesixty', [])
+		.config(config)
+		.filter('parseDate',parseDateFilter)
+		.filter('numberFixedLen',numberFixedLength)
 		.filter('trustUrl', function ($sce) {
 			return function(url) {
 				return $sce.trustAsResourceUrl(url);
 			};
 		});
 
-    /** @ngInject */
-    function config($stateProvider, msNavigationServiceProvider, mesentitlementProvider, $sceDelegateProvider)
-    {
-
-        function gst(name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-            }
-            //debugger;
-            return null;
-        }
-        /** Check for Super admin */
-        var isSuperAdmin = gst('isSuperAdmin');
-        /** Check for Super admin - END */ 
-
-        $stateProvider
-            .state('app.threesixty', {
-                url    : '/threesixty',
-                views  : {
-                    'threesixty@app': {
-                        templateUrl: 'app/main/360/threesixty.html',
-                        controller : 'ThreeSixtyController as vm'
-                    }
-                },
-                resolve: {
+	/** @ngInject */
+	function config($stateProvider, msNavigationServiceProvider, mesentitlementProvider, $sceDelegateProvider)
+	{
+		$stateProvider
+			.state('app.threesixty', {
+				url    : '/threesixty',
+				views  : {
+					'threesixty@app': {
+						templateUrl: 'app/main/360/threesixty.html',
+						controller : 'ThreeSixtyController as vm'
+					}
+				},
+				resolve: {
 					security: ['$q','mesentitlement','$timeout','$rootScope','$state','$location', function($q,mesentitlement,$timeout,$rootScope,$state, $location){
 						return $q(function(resolve, reject) {
 							$timeout(function() {
-								if ($rootScope.isBaseSet2 && isSuperAdmin != 'true') {
+								if ($rootScope.isBaseSet2) {
 									resolve(function () {
 										var entitledStatesReturn = mesentitlement.stateDepResolver('threesixty');
 
@@ -69,19 +53,17 @@
 							});
 						});
 					}]
-                },
-                bodyClass: 'threesixty'
-            });
+				},
+				bodyClass: 'threesixty'
+			});
 
-        // Navigation
+		// Navigation
 
-        if(isSuperAdmin != 'true'){
-            msNavigationServiceProvider.saveItem('threesixty', {
-                title    : '360',
-                state    : 'app.threesixty',
-                weight   : 7
-            });
-        }
+		msNavigationServiceProvider.saveItem('threesixty', {
+			title    : '360',
+			state    : 'app.threesixty',
+			weight   : 7
+		});
 
 		$sceDelegateProvider.resourceUrlWhitelist([
 			// Allow same origin resource loads.
@@ -99,26 +81,26 @@
 			"http://ccresourcegrpdisks974.blob.core.windows.net/**",
 			"https://ccresourcegrpdisks974.blob.core.windows.net/**"
 		]);
-    }
+	}
 
-    function parseDateFilter(){
-        return function(input){
-            return new Date(input);
-        };
-    }
+	function parseDateFilter(){
+		return function(input){
+			return new Date(input);
+		};
+	}
 
-    function numberFixedLength(){
-      return function (n, len) {
-        var num = parseInt(n, 10);
-        len = parseInt(len, 10);
-        if (isNaN(num) || isNaN(len)) {
-          return n;
-        }
-        num = ''+num;
-        while (num.length < len) {
-          num = '0'+num;
-        }
-        return num;
-      };
-    }
+	function numberFixedLength(){
+		return function (n, len) {
+			var num = parseInt(n, 10);
+			len = parseInt(len, 10);
+			if (isNaN(num) || isNaN(len)) {
+				return n;
+			}
+			num = ''+num;
+			while (num.length < len) {
+				num = '0'+num;
+			}
+			return num;
+		};
+	}
 })();
