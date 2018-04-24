e71eae552c8c30410571a07ec31d4a50844c6eed
gihan herath <gihan@duosoftware.com>
2018-04-24 12:26:02 +0530

    billing address field mandatory issue fixed

--- 360/threesixty.controller.js
+++ 360/threesixty.controller.js
@@ -2003,6 +2003,10 @@
 				})
 			}
 		}
+		
+		$scope.addressChanged = function () {
+		  $scope.createProfile.country = document.getElementById('country').value;
+		}
 
 		$scope.showInpageReadpane = false;
 		$scope.switchInfoPane = function (state, profile) {
